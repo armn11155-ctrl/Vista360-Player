8a428b165b2d3a1acb5044956330bf9e69921ef4
Armando N. <armn.11155@gmail.com>
2026-06-28 17:55:08 -0500

    Player: enganchar setupRealViewportHeight al arranque

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/app.css";
 import { setupFullscreenOnTouch } from "./utils/fullscreen";
+import { setupRealViewportHeight } from "./utils/viewport-height";
+
+setupRealViewportHeight();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
