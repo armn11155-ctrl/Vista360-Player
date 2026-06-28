4b35be2c8690ffeffebdf4819fa67a2f8045e1be
Armando N. <armn.11155@gmail.com>
2026-06-28 16:43:47 -0500

    Player: enganchar setupFullscreenOnTouch en el arranque

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/app.css";
+import { setupFullscreenOnTouch } from "./utils/fullscreen";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+setupFullscreenOnTouch();
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
