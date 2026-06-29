44565f848f62a60c2e635610602b12fc4c572183
Armando N. <armn.11155@gmail.com>
2026-06-28 21:12:03 -0500

    Icono de flecha de regreso reutilizable

---
+++ src/components/BackChevron.tsx
@@ -0,0 +1,7 @@
+export default function BackChevron() {
+  return (
+    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#111827" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M15 18l-6-6 6-6" />
+    </svg>
+  );
+}
